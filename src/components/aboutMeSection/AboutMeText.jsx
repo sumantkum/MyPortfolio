a88b4702409f3158d90295ac2c4255c242df0fb0
Sumant Kumar <sumantpandit993127@gmail.com>
2025-05-03 20:21:46 +0530

    updated by me

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -7,7 +7,7 @@ const AboutMeText = () => {
       <p>
         I’m Sumant kumar pandit, a web developer instructor with a passion for teaching
         and coding. I specialize in React and front-end development, helping
-        students build real-world projects and master modern web technologies. CodeNest, where I create tutorials and
+        students build real-world projects and master modern web technologies.CodeNest, where I create tutorials and
         courses to guide aspiring developers in their journey toward successful
         careers in tech. Outside of coding, I enjoy continuous learning and
         sharing knowledge to inspire others to achieve their goals.
